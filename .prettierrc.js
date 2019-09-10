32bacda613f91ab5a9a637d4a5067e7603c1dcf4
devin <devin.dai@1quant.com>
2019-09-10 17:04:47 +0800

    1、实现vue + typescript + eslint + prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  jsxBracketSameLine: true,
+  htmlWhitespaceSensitivity: "ignore",
+  printWidth: 120, // 一行的字符数，如果超过会进行换行，默认为80
+  tabWidth: 2, // 一个tab代表几个空格数，默认为80
+  useTabs: false, // 是否使用tab进行缩进，默认为false，表示用空格进行缩减
+  semi: true, // 行位是否使用分号，默认为true
+  trailingComma: "none", // 是否使用尾逗号，有三个可选值"<none|es5|all>"
+  bracketSpacing: true // 对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
+};
